487d8e38f091df8579a7a7c42085ad35442ed6ae
Rodrigo <rtirapeg@itba.edu.ar>
2019-04-19 14:04:11 -0300

    - Implementación del ejercicio 1 (sincronización de tareas)

--- so/src/main.c
+++ so/src/main.c
@@ -21,17 +21,78 @@
  *********************/
 
 /*
- * Example to test tasks priority scheduling
+ * Ejercicio 1
  */
 
-OS_TASK_CREATE(Tarea_1, STACK_SIZE_BYTES, Task1, (void *) 0x11223344, TASK_PRIORITY_HIGH);
-OS_TASK_CREATE(Tarea_2, STACK_SIZE_BYTES, Task2, (void *) 0x11223344, TASK_PRIORITY_LOW);
-OS_TASK_CREATE(Tarea_3, STACK_SIZE_BYTES, Task3, (void *) 0x11223344, TASK_PRIORITY_MEDIUM);
-OS_TASK_CREATE(Tarea_4, STACK_SIZE_BYTES, Task4, (void *) 0x11223344, TASK_PRIORITY_LOW);
-OS_TASK_CREATE(Tarea_5, STACK_SIZE_BYTES, Task5, (void *) 0x11223344, TASK_PRIORITY_LOW);
-OS_TASK_CREATE(Tarea_6, STACK_SIZE_BYTES, Task6, (void *) 0x11223344, TASK_PRIORITY_HIGH);
-OS_TASK_CREATE(Tarea_7, STACK_SIZE_BYTES, Task7, (void *) 0x11223344, TASK_PRIORITY_LOW);
-OS_TASK_CREATE(Tarea_8, STACK_SIZE_BYTES, Task8, (void *) 0x11223344, TASK_PRIORITY_MEDIUM);
+OS_TASK_CREATE(Tarea_1, STACK_SIZE_BYTES, buttonSampling_Task, (void *) 0x11223344, TASK_PRIORITY_IDLE + 1);
+OS_TASK_CREATE(Tarea_2, STACK_SIZE_BYTES, ledBlink_Task, (void *) 0x11223344, TASK_PRIORITY_IDLE + 2);
+
+OS_SEMPHR_CREATE(Sem_T1_T2);
+
+/*************
+ * Variables *
+ ************/
+static tick_t g_samplingTickCount;
+
+/*********
+ * Tasks *
+ ********/
+void * buttonSampling_Task(void * params)
+{
+   #define DEBOUNCE  40 // In ms
+
+   tick_t t0, t1, diff;
+
+   while(true)
+   {
+      if(true == Board_BUTTON_isPressed(TEC_1))
+      {
+         t0 = os_task_getTickCount();
+
+         while(true == Board_BUTTON_isPressed(TEC_1))
+            ;
+
+         t1 = os_task_getTickCount();
+
+         diff = (t1 > t0 ? t1 - t0 : 0xFFFFFFFF - t0 + t1);
+
+         if((DEBOUNCE / OS_TICK_PERIOD_MS) < diff)
+         {
+        	 /* @todo lock with mutex here */
+        	 g_samplingTickCount = diff;
+        	 /* @todo unlock with mutex here */
+
+        	 os_semphr_post(Sem_T1_T2);
+         }
+      }
+   }
+
+   return NULL;
+}
+
+void * ledBlink_Task(void * params)
+{
+   tick_t samplingTickCount;
+
+   while(true)
+   {
+	 if(true == os_semphr_wait(Sem_T1_T2, OS_TICK_MAX))
+	 {
+		/* @todo lock with mutex here */
+		samplingTickCount = g_samplingTickCount;
+		/* @todo unlock with mutex here */
+
+		if(0 < samplingTickCount)
+		{
+			Board_LED_Set(LED_BLUE, true);
+			os_task_delay(samplingTickCount / OS_TICK_PERIOD_MS);
+			Board_LED_Set(LED_BLUE, false);
+		}
+	 }
+   }
+
+   return NULL;
+}
 
 /************************
  *	Private functions	*
@@ -41,86 +102,6 @@ static void initHardware(void)
 	// Set up and initialize all required blocks and
     // functions related to the board hardware
 	Board_Init();
-}
-static void * Task1(void *arg)
-{
-   while(1)
-   {
-	   os_task_delay(15);
-      //__WFI();
-   }
-
-   return NULL;
-}
-static void * Task2(void *arg)
-{
-   while(1)
-   {
-	  os_task_delay(15);
-      //__WFI();
-   }
-
-   return NULL;
-}
-static void * Task3(void *arg)
-{
-   while(1)
-   {
-	   os_task_delay(15);
-      //__WFI();
-   }
-
-   return NULL;
-}
-static void * Task4(void *arg)
-{
-   while(1)
-   {
-	  os_task_delay(15);
-      //__WFI();
-   }
-
-   return NULL;
-}
-static void * Task5(void *arg)
-{
-   while(1)
-   {
-	  os_task_delay(15);
-      //__WFI();
-   }
-
-   return NULL;
-}
-static void * Task6(void *arg)
-{
-   while(1)
-   {
-	  os_task_delay(15);
-      //__WFI();
-   }
-
-   return NULL;
-}
-static void * Task7(void *arg)
-{
-   while(1)
-   {
-	  os_task_delay(15);
-      //__WFI();
-   }
-
-   return NULL;
-}
-static void * Task8(void *arg)
-{
-   while(1)
-   {
-	   os_task_delay(15);
-      //__WFI();
-   }
-
-   return NULL;
 }
 
 /************************
